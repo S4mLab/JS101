835a8b6855b1caf5815f8e792df5466fb6f2eaba
sam <samvbui1995@gmail.com>
2022-03-15 09:14:13 +1100

    return position value of letters in a string

---
+++ alphabetPosition.js
@@ -0,0 +1,48 @@
+const alphabetPosition = (inputStr) => {
+  // convert to lowercase
+  const lowercasedInput = inputStr.toLowerCase();
+  // create an array for all alphabet letters. The position of each one will be their index + 1
+  const alphabetLettersList = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  // break the string into an array of letters
+  const inputLettersList = lowercasedInput.split('');
+  // loop though input letter array
+  const positionValuesList = inputLettersList.map((letter) => {
+    // if the element is in the alphabet letters list
+    if (alphabetLettersList.includes(letter)) {
+      // replace it with its positions value in the alphabet
+      return alphabetLettersList.indexOf(letter);
+    } else {
+      // return nothing
+    }
+  });
+  console.log(positionValuesList);
+};
+
+alphabetPosition('CaN YoU hEar mE');
