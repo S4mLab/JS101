1013491aedac4684fb5d74854204bd412297af7d
sam <samvbui1995@gmail.com>
2022-03-14 22:32:47 +1100

    removing vowels in strings

---
+++ disemvowel.js
@@ -0,0 +1,17 @@
+/*
+write a function that takes a string and return a new string with all vowels removed.
+ie: 
+input: "This website is for losers LOL!" 
+ouput: "Ths wbst s fr lsrs LL!"
+
+for this kata y isn't considered a vowel.
+*/
+
+const disemvowel = (inputStr) => {
+  // have an array that contains all the vowels
+  const vowelsArray = ['a', 'e', 'i', 'o', 'u'];
+  // take the string, split them into array of words
+  const wordsArray = inputStr.map((word) => {});
+  // loop through each word
+  // check for vowels and remove them, use filter()
+};
